3abccf7cfc8ff7bf04eaee30e24f5cb5fe4fe76f
Clive <Cliveswe@users.noreply.github.com>
2025-05-16 11:16:18 +0200

    Set up the middle ware and added the HTTP request pipeline.

--- WatchesApp.Web/Program.cs
+++ WatchesApp.Web/Program.cs
@@ -4,10 +4,17 @@ public class Program
 {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Register services to the container that supports MVC pattern.
+        builder.Services.AddControllersWithViews();
+
+        // Build the application where we can configure the HTTP request pipeline.
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        // Register middle-ware for the HTTP request pipeline.
+        app.MapControllers();
 
+        // Start the application and listen for incoming HTTP requests.
         app.Run();
     }
 }
