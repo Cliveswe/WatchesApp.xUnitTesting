2681f9848bd4304baccdb297e2765b771c739e9a
Clive <Cliveswe@users.noreply.github.com>
2025-05-16 11:29:58 +0200

    Added an interface class for a repository of mock data.

---
+++ WatchesApp.Web/Models/IWatchRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace WatchesApp.Web.Models;
+
+public interface IWatchRepository
+{
+    IEnumerable<Watch> GetAllWatches();
+    Watch? GetWatchById(int id);
+    void AddWatch(Watch watch);
+    void UpdateWatch(Watch watch);
+    void DeleteWatch(int id);
+}
