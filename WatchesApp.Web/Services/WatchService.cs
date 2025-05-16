ff0f5f5d737b1e1ef9e731adb2a2ed3be57bb572
Clive <Cliveswe@users.noreply.github.com>
2025-05-16 11:38:20 +0200

    Created a services folder that contains the application watch services. At this stage the watch services is a mock data file.

---
+++ WatchesApp.Web/Services/WatchService.cs
@@ -0,0 +1,26 @@
+﻿using WatchesApp.Web.Models;
+
+namespace WatchesApp.Web.Services;
+
+public class WatchService : IWatchRepository
+{
+    public void AddWatch(Watch watch) {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteWatch(int id) {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Watch> GetAllWatches() {
+        throw new NotImplementedException();
+    }
+
+    public Watch? GetWatchById(int id) {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateWatch(Watch watch) {
+        throw new NotImplementedException();
+    }
+}
